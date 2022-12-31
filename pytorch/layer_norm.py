3a508d24878e37c628b801a2672e2de9854ea685
huangqianfei <huangqianfei@tju.edu.cn>
2022-12-31 22:38:21 +0800

    add batch layer norm

---
+++ pytorch/layer_norm.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch
+if __name__ == '__main__':
+  norm = nn.LayerNorm([4,5], elementwise_affine=True)
+  inputs = torch.FloatTensor([i for i in range(60)]).reshape(3,4,5)
+
+  norm = nn.LayerNorm(5, elementwise_affine=True)
+  inputs = torch.FloatTensor([i for i in range(20)]).reshape(4,5)
+
+  output = norm(inputs)
+  print(output)
+  '''
+  	tensor([[-1.3416, -0.4472,  0.4472,  1.3416],
+    		[-1.3416, -0.4472,  0.4472,  1.3416]],
+   				grad_fn=<NativeLayerNormBackward>)
+  '''
+  print(norm.weight.shape)
